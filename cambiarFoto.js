997e72919545db505c1c5a85adf8f9075e2567a3
Zguedest <zucelys.zge@gmail.com>
2023-01-26 13:59:09 +0100

    evento mousover

---
+++ cambiarFoto.js
@@ -0,0 +1,10 @@
+
+
+let imagen = document.getElementById("img");
+
+imagen.addEventListener("mouseover", (event)=>{
+
+    console.log(event)
+    imagen.style.backgroundImage= "url(./img/zu.jpeg)";
+
+} )
