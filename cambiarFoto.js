1d8f6d65e87719d1bbe6b11eb7c63ef3095036f0
xabier iglesias <xiauretamendi@gmail.com>
2023-01-26 14:07:33 +0100

    evento mouseout

--- cambiarFoto.js
+++ cambiarFoto.js
@@ -4,7 +4,14 @@ let imagen = document.getElementById("img");
 
 imagen.addEventListener("mouseover", (event)=>{
 
-    console.log(event)
     imagen.style.backgroundImage= "url(./img/zu.jpeg)";
+
+} )
+
+
+imagen.addEventListener("mouseout", (event)=>{
+
+    console.log(event)
+    imagen.style.backgroundImage= "url(./img/fotoxabier.jpg)";
 
 } )
